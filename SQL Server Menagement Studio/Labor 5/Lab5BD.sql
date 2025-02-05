411897d334d0892f2d9a573f97f5e7f4f0de4aed
Hrihor Mihaela <150060790+mihaelahrihor@users.noreply.github.com>
2025-02-05 04:40:59 -1000

    Add files via upload

---
+++ SQL Server Menagement Studio/Labor 5/Lab5BD.sql
@@ -0,0 +1,146 @@
+﻿--ex1 
+CREATE TABLE Ta (
+    idA INT PRIMARY KEY,
+    a2 INT UNIQUE,
+    a3 INT
+);
+ 
+CREATE TABLE Tb (
+    idB INT PRIMARY KEY,
+    b2 INT,
+    b3 INT
+);
+ 
+CREATE TABLE Tc (
+    idC INT PRIMARY KEY,
+    idA INT,
+    idB INT,
+    FOREIGN KEY (idA) REFERENCES Ta(idA),
+    FOREIGN KEY (idB) REFERENCES Tb(idB)
+);
+ 
+
+CREATE PROCEDURE PopulareTabele
+AS
+BEGIN
+    SET NOCOUNT ON;
+ 
+    -- Golirea tabelelor folosind DELETE
+    DELETE FROM Tc;
+    DELETE FROM Tb;
+    DELETE FROM Ta;
+ 
+    DECLARE @i INT = 1; -- contor pt bucle
+  --variabila pt stocarea valorilor generate aleator
+    DECLARE @randA INT, @randB INT;
+ 
+    -- Populare Ta cu 10.000 de randuri
+    WHILE @i <= 10000
+    BEGIN
+        -- Generam un nr aleator pt a3 si valoare unica pt a2
+        SET @randA = FLOOR(RAND() * 10000); 
+        INSERT INTO Ta (idA, a2, a3)
+        VALUES (@i, @i * 10, FLOOR(RAND() * 10000));-- a2 este unic prin formula `@i * 10'
+        SET @i = @i + 1; -- incrementam contorul
+    END;
+ 
+    -- Resetare contor
+    SET @i = 1;
+ 
+    -- Populare Tb cu 3.000 de randuri
+    WHILE @i <= 3000
+    BEGIN
+    -- generam un nr aleator pt 'b2'
+        SET @randB = FLOOR(RAND() * 10000); -- Generare nr aleator pentru b2
+        INSERT INTO Tb (idB, b2, b3)
+        -- Valorile sunt generate aleator pentru b2 si b3
+    VALUES (@i, @randB, FLOOR(RAND() * 10000));
+        SET @i = @i + 1; -- incrementam contorul
+  END;
+ 
+    -- Resetare contor
+    SET @i = 1;
+ 
+    -- Populare Tc cu 30.000 de randuri
+    WHILE @i <= 30000
+    BEGIN
+    -- Generam chei externe aleatoare care refera Ta si Tb
+        INSERT INTO Tc (idC, idA, idB)
+        VALUES (
+            @i,
+            FLOOR(RAND() * 10000) + 1, -- idA este un nr intre 1 si 10.000
+            FLOOR(RAND() * 3000) + 1   -- idB este un nr intre 1 si 3.000
+        );
+        SET @i = @i + 1; -- incrementam contorul
+    END;
+ 
+    SET NOCOUNT OFF;
+END;
+ 
+ 
+EXEC PopulareTabele;
+ 
+ 
+SELECT COUNT(*) FROM Ta;
+SELECT COUNT(*) FROM Tb;
+SELECT COUNT(*) FROM Tc;
+ 
+ 
+--a
+-- Index la tabela TA
+SELECT * 
+FROM sys.indexes
+WHERE object_id = OBJECT_ID('Ta');
+ 
+--Clustered Index Scan
+SELECT * 
+FROM Ta
+WHERE a3 > 9990;
+ 
+--Clustered Index Seek
+SELECT * 
+FROM Ta
+WHERE idA = 5000;
+ 
+--Nonclustered Index Scan
+SELECT * 
+FROM Ta
+WHERE a2 BETWEEN 1000 AND 2000;
+ 
+--Nonclustered Index Seek
+SELECT * 
+FROM Ta
+WHERE a2 = 10000;
+ 
+--b
+SELECT idA, a2
+FROM Ta
+WHERE a3 = 500;
+ 
+--c 
+SELECT * 
+FROM Tb
+WHERE b2 = 40;
+ 
+CREATE NONCLUSTERED INDEX idx_b2 ON Tb (b2);
+ 
+DROP INDEX idx_b2 ON Tb;
+ 
+SELECT * 
+FROM Tb
+WHERE b2 = 40;
+ 
+--d
+SELECT Tc.idC, Ta.a2 
+FROM Tc
+INNER JOIN Ta ON Tc.idA = Ta.idA
+WHERE Tc.idA = 1000;
+ 
+SELECT Tc.idC, Tb.b2
+FROM Tc
+INNER JOIN Tb ON Tc.idB = Tb.idB
+WHERE Tc.idB = 2000;
+ 
+ 
+CREATE NONCLUSTERED INDEX idx_idA ON Tc (idA);
+CREATE NONCLUSTERED INDEX idx_idB ON Tc (idB);
---
+++ SQL Server Menagement Studio/Labor 5/Lab5BDproviz.sql
@@ -0,0 +1,141 @@
+CREATE TABLE Ta (
+    idA INT IDENTITY(1,1) PRIMARY KEY,
+    a2 INT UNIQUE,
+    a3 INT
+);
+
+CREATE TABLE Tb (
+    idB INT IDENTITY(1,1) PRIMARY KEY,
+    b2 INT,
+    b3 INT
+);
+
+CREATE TABLE Tc (
+    idC INT IDENTITY(1,1) PRIMARY KEY,
+    idA INT,
+    idB INT,
+    FOREIGN KEY (idA) REFERENCES Ta(idA),
+    FOREIGN KEY (idB) REFERENCES Tb(idB)
+);
+
+CREATE PROCEDURE InsertData
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- 
+    DECLARE @i INT = 1;
+    WHILE @i <= 10000
+    BEGIN
+        INSERT INTO Ta (a2, a3) VALUES (@i * 10, ABS(CHECKSUM(NEWID())) % 1000);
+        SET @i = @i + 1;
+    END;
+
+    -- 
+    DECLARE @j INT = 1;
+    WHILE @j <= 3000
+    BEGIN
+        INSERT INTO Tb (b2, b3) VALUES (@j * 5, ABS(CHECKSUM(NEWID())) % 500);
+        SET @j = @j + 1;
+    END;
+
+    --
+    DECLARE @k INT = 1;
+    WHILE @k <= 30000
+    BEGIN
+        INSERT INTO Tc (idA, idB) VALUES (
+            1 + ABS(CHECKSUM(NEWID())) % 10000, 
+            1 + ABS(CHECKSUM(NEWID())) % 3000  
+        );
+        SET @k = @k + 1;
+    END;
+END;
+
+EXEC InsertData;
+
+--ex2
+-- a
+SELECT 
+    t.name AS TableName,
+    i.name AS IndexName,
+    i.type_desc AS IndexType,
+    c.name AS ColumnName
+FROM 
+    sys.indexes i
+INNER JOIN 
+    sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
+INNER JOIN 
+    sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
+INNER JOIN 
+    sys.tables t ON i.object_id = t.object_id
+WHERE 
+    t.name = 'Ta';
+
+
+CREATE NONCLUSTERED INDEX idx_a3 ON Ta(a3);
+
+
+--scan clustered
+SELECT * FROM Ta;
+
+--seek clustered
+SELECT * FROM Ta WHERE idA = 500;
+
+--nonclustered scan
+SELECT a3 FROM Ta WHERE a3 < 50;
+
+--nonclustered seek
+SELECT a3 FROM Ta WHERE a3 = 100;
+
+--b
+CREATE NONCLUSTERED INDEX idx_a2 ON Ta(a2);
+
+SELECT idA, a3 
+FROM Ta 
+WHERE a2 = 50;
+
+--c
+SELECT * 
+FROM Tb 
+WHERE b2 = 100;
+
+CREATE NONCLUSTERED INDEX idx_b2 ON Tb(b2);
+
+--d
+SELECT Tc.idC, Ta.a2, Ta.a3
+FROM Tc
+INNER JOIN Ta ON Tc.idA = Ta.idA
+WHERE Ta.a2 = 100;
+
+SELECT Tc.idC, Tb.b2, Tb.b3
+FROM Tc
+INNER JOIN Tb ON Tc.idB = Tb.idB
+WHERE Tb.b2 = 50;
+
+CREATE NONCLUSTERED INDEX idx_idA ON Tc(idA);
+
+CREATE NONCLUSTERED INDEX idx_idB ON Tc(idB);
+
+
+SELECT Tc.idC, Ta.a2, Ta.a3
+FROM Tc
+INNER JOIN Ta ON Tc.idA = Ta.idA
+WHERE Ta.a2 = 100;
+
+SELECT Tc.idC, Tb.b2, Tb.b3
+FROM Tc
+INNER JOIN Tb ON Tc.idB = Tb.idB
+WHERE Tb.b2 = 50;
+
+
+
+
+
+
+
+
+
+
+
+
+
